085e7f988f288c60f25849e69c4112114e40d8c3
Jan Bucher <jan@bucher.cloud>
2021-04-02 12:45:00 +0200

    frontend: Add angular boilerplace project with material design enabled

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverviewComponent } from './overview/overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
