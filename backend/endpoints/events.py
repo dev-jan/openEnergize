02599a8915c3daf8d3e9d9e6f37124baf0c2ee8f
Jan Bucher <jan@bucher.cloud>
2021-04-26 10:07:19 +0200

    backend: Extend events endpoint to contain more detailed fields

--- backend/endpoints/events.py
+++ backend/endpoints/events.py
@@ -3,7 +3,23 @@ from flask_restx import Namespace, Resource, fields
 api = Namespace('events', description='Events')
 
 event = api.model('Event', {
-    'text': fields.String(required=True, description='The text of the event')
+    'timestamp': fields.String(
+        required=True,
+        description='Timestamp of the event'
+    ),
+    'level': fields.String(
+        required=True,
+        description='Loglevel. Possible values: \
+        (DEBUG, INFO, WARNING, ERROR, CRITICAL, NONE)'
+    ),
+    'name': fields.String(
+        required=True,
+        description='Module name of the event'
+    ),
+    'text': fields.String(
+        required=True,
+        description='The text of the event'
+    )
 })
 
 
@@ -17,6 +33,13 @@ class EventList(Resource):
             lines = file.readlines()
             for line in lines:
                 line = line.rstrip('\n')
-                events.append({'text': line})
+                fields = line.split(' ')
+                if len(fields) > 5 and fields[3] != 'werkzeug':
+                    singleEvent = {}
+                    singleEvent['timestamp'] = fields[0] + " " + fields[1]
+                    singleEvent['level'] = fields[2]
+                    singleEvent['name'] = fields[3]
+                    singleEvent['text'] = ' '.join(fields[4:])
+                    events.append(singleEvent)
         events.reverse()
         return events
