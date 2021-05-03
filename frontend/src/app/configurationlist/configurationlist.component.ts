a7846b307b4b09e5a1446e0d371a94a188da7105
Jan Bucher <jan@bucher.cloud>
2021-05-03 08:07:51 +0200

    frontend: Add backend error handling for configurationlist component

--- frontend/src/app/configurationlist/configurationlist.component.ts
+++ frontend/src/app/configurationlist/configurationlist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigurationService } from '../configuration.service';
 import { Configuration } from '../models/Configuration';
 import { environment } from '../../environments/environment';
+import { PartialObserver } from 'rxjs';
 
 @Component({
   selector: 'app-configurationlist',
@@ -12,12 +13,16 @@ export class ConfigurationlistComponent implements OnInit {
 
   configuration?: Configuration;
   environmentConfig = environment;
+  errorMessage?: string;
 
   constructor(private configurationService: ConfigurationService) { }
 
   ngOnInit(): void {
     this.configurationService.getFullConfiguration()
-        .subscribe(data => this.configuration = data);
+        .subscribe({
+          next: data => this.configuration = data,
+          error: error => this.errorMessage = error.message
+        } as PartialObserver<Configuration>);
   }
 
 }
